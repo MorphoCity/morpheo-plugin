Compute orthogonality - fix connectivity computation

src/morpheo/core/builders/angles.py
@@ -2,6 +2,7 @@
 """ Partition tools 
 """
 from collections import namedtuple
+from numpy import pi
 import numpy as np
 
 #-------------------------------
@@ -149,7 +150,7 @@ def angle_from_azimuth( az1, az2 ):
     """ Compute angle between two azimuths
     """
     angle = abs(az1-az2)
-    if angle > np.pi:
-        angle = 2*np.pi - angle
-    return abs( angle - np.pi )
+    if angle > pi:
+        angle = 2*pi - angle
+    return abs( angle - pi )
 

src/morpheo/core/builders/ways.py
@@ -6,6 +6,10 @@ from __future__ import print_function
 import os
 import logging
 
+
+import numpy as np
+
+from numpy import sin
 from functools import partial
 from itertools import takewhile
 from ..logger import log_progress
@@ -17,26 +21,48 @@ from .sql import SQL, execute_sql, delete_table
 class WayBuilder(object):
 
     def __init__(self, conn):
+
+       from qgis.core import QgsPoint
        self._conn = conn
+
+       # Define helper functions
+       p1,p2 = QgsPoint(), QgsPoint()
+       def azimuth(x1,y1,x2,y2):
+           p1.set(x1,y1)
+           p2.set(x2,y2)
+           return p1.azimuth(p2) / 180.0 * np.pi
+
+       def distance(x1,y1,x2,y2):
+           p1.set(x1,y1)
+           p2.set(x2,y2)
+           return np.sqrt(p1.sqrDist(p2))
+
+       self.azimuth  = azimuth
+       self.distance = distance
 
     def build_ways(self, threshold):
         """ Compute ways
 
             Pair edges for each place then resolve pairing as a partitioning
-            set: each resulting classes will be a way. 
+            set: each resulting classes will be a way.
+
+            :params threshold: The angle threshold (in radian) for pairing edges at each place.
         """
-        import numpy as np
-        from qgis.core import QgsPoint
         from .angles import (create_partition, resolve, update, num_partitions, get_index_table,
                              create_matrix, next_argmin, get_value, pop_args, get_remaining_elements,
                              angle_from_azimuth)
-                                 
+       
+        # Dereference helper functions
+        azimuth  = self.azimuth
+        distance = self.distance
+
         cur = self._conn.cursor()
 
         # Get the (max) number of edges and places
         max_edges  = cur.execute("SELECT Max(OGC_FID) from place_edges").fetchone()[0]
         max_places = cur.execute("SELECT Max(OGC_FID) from places").fetchone()[0] 
 
+        # Get the entry vector for edges in each place
         rows = cur.execute(SQL("""SELECT 
             pl, fid, ST_X(p1), ST_Y(p1), ST_X(p2), ST_Y(p2)
             FROM (
@@ -56,17 +82,6 @@ class WayBuilder(object):
             ORDER BY pl
         """)).fetchall()
 
-        p1,p2 = QgsPoint(), QgsPoint()
-        def azimuth(x1,y1,x2,y2):
-            p1.set(x1,y1)
-            p2.set(x2,y2)
-            return p1.azimuth(p2) / 180.0 * np.pi
-
-        def distance(x1,y1,x2,y2):
-            p1.set(x1,y1)
-            p2.set(x2,y2)
-            return np.sqrt(p1.sqrDist(p2))
-
         def deviation( az1, x1, y1, az2, x2, y2 ):
             """ Computei deviation  coefficient 
 
@@ -75,8 +90,8 @@ class WayBuilder(object):
             a1 = azimuth(x1,y1,x2,y2)
             a2 = azimuth(x2,y2,x1,y1)
             d  = distance(x1,y1,x2,y2)
-            return (abs( np.sin(angle_from_azimuth(az1,a1))) +
-                    abs( np.sin(angle_from_azimuth(az2,a2)))) * d 
+            return (abs( sin(angle_from_azimuth(az1,a1))) +
+                    abs( sin(angle_from_azimuth(az2,a2)))) * d 
 
         edges_az = [(r[0],r[1],azimuth(*r[2:6]),r[2],r[3]) for r in rows]
 
@@ -90,7 +105,9 @@ class WayBuilder(object):
         # Compute candidates pair for each places
         # Each edge is given a way number, 
         # Places with degree=2 are automatically paired together
-        
+
+        # Iterate through places, returning the set of edges/vector for
+        # that place
         def places():
             s = 0
             while s<len(rows):
@@ -116,7 +133,9 @@ class WayBuilder(object):
                     raise BuilderError("Cannot set more than two ends for one way") 
                 endplaces[fid] = place
 
-        # Way partition
+        # Way partition: resolve each pair by assigning them
+        # to the same equivalent class. Partition are computed 
+        # by resolving transitive relationship. 
         ways = create_partition(max_edges+1)
         def add_pair( e1, e2 ):
             resolve(ways,e1[1],e2[1])
@@ -132,7 +151,6 @@ class WayBuilder(object):
             n = len(edges)
             num_places = num_places+1
             if n>2:
-                # Compute pairing
                 # Compute angles between edges
                 angles = compute_angles(edges)
                 # compute coeffs between edges
@@ -162,7 +180,10 @@ class WayBuilder(object):
         return num_ways
         
     def _build_way_table( self, cur, ways, distances, startplaces, endplaces ):
-        """ Write back ways
+        """ Write back way partition
+
+            Create a table holding the partition mapping for edges. The table
+            also contains the distance corrections.
         """
         cur.execute(SQL("DELETE FROM way_partition"))
         cur.executemany(SQL("INSERT INTO way_partition(PEDGE,WAY,DIST,START_PL,END_PL) SELECT ?,?,?,?,?"),
@@ -187,9 +208,84 @@ class WayBuilder(object):
         logging.info("Computing local attributes")
         execute_sql(self._conn, "way_local.sql")
 
+        # Compute orthogonality
         if orthogonality:
-            logging.info("Computing orthogonality")
+            self.compute_orthogonality()
 
+        self._conn.commit()
+    
+    def compute_orthogonality(self):
+        """ Compute orthogonality
+        """
+        from .angles import angle_from_azimuth
 
+        logging.info("Computing orthogonality")
 
+        f_azimuth = self.azimuth
+
+        cur  = self._conn.cursor()
+        rows = cur.execute(SQL("""SELECT 
+            pl, way, ST_X(p1), ST_Y(p1), ST_X(p2), ST_Y(p2)
+            FROM (
+                SELECT 
+                START_PL AS pl,
+                WAY AS way,
+                ST_StartPoint(GEOMETRY) AS p1, 
+                ST_PointN(GEOMETRY,2) AS p2
+                FROM place_edges
+            UNION ALL
+                SELECT
+                END_PL AS pl,
+                WAY AS way,
+                ST_EndPoint(GEOMETRY) AS p1, 
+                ST_PointN(GEOMETRY, ST_NumPoints(GEOMETRY)-1) AS p2
+                FROM place_edges)
+            ORDER BY pl
+        """)).fetchall()
+
+        # compute azimuths
+        way_places = [(r[0],r[1],f_azimuth(*r[2:])) for r in rows]
+
+        # Compute all angles for each pairs of way 
+        # for each places
+
+        def places():
+            s = 0
+            while s<len(rows):
+                p = way_places[s][0]
+                l = list(takewhile(lambda x: x[0]==p, way_places[s:]))
+                s = s+len(l)
+                yield p,l
+        
+        def compute_angles():
+            for place, ways in places():
+                n = len(ways)
+                if n==1:
+                    continue
+                for i in xrange(n-1):
+                    w1 = ways[i]
+                    i1 = w1[1]
+                    for j in xrange(i+1,n):
+                        w2 = ways[j]
+                        i2 = w2[1]  
+                        if i1 != i2:
+                            angle = sin( angle_from_azimuth(w1[2],w2[2]) )
+                            yield (place,angle,min(i1,i2),max(i1,i2))
+
+        # Build way_angles table
+        cur.execute(SQL("DELETE FROM way_angles"))
+        cur.executemany(SQL("INSERT INTO way_angles(PLACE,ANGLE,WAY1,WAY2) SELECT ?,?,?,?"),
+                [(pl,angle,way1,way2) for pl,angle,way1,way2 in compute_angles()])
+
+        # Update orthogonality
+        cur.execute(SQL("""UPDATE ways SET ORTHOGONALITY = (
+            SELECT Sum(inner)/ways.CONNECTIVITY FROM (
+            SELECT Min(a) AS inner FROM (
+                SELECT PLACE AS p, ANGLE AS a, WAY2 AS w
+                FROM way_angles WHERE WAY1=ways.WAY_ID
+                UNION ALL
+                SELECT PLACE AS p, ANGLE AS a, WAY1 AS w
+                FROM way_angles WHERE WAY2=ways.WAY_ID)
+                GROUP BY p,w)
+           ) WHERE ways.CONNECTIVITY > 0"""))
 