feb8ec0a820315063f27885716319486d2f6607a
rldhont <rldhont@gmail.com>
2016-08-30 13:36:26 +0200

    QGIS Plugin: Add UI

--- src/morpheo/__init__.py
+++ src/morpheo/__init__.py
@@ -5,6 +5,11 @@
 # -----------------------
 
 def classFactory(iface):
-    from gui.gui import Gui
-    return Gui(iface)
+    import os, sys
+    cmd_folder = os.path.join(os.path.dirname(__file__),'gui')
+    if cmd_folder not in sys.path:
+        sys.path.insert(0, cmd_folder)
+
+    from gui.MorpheoPlugin import MorpheoPlugin
+    return MorpheoPlugin(iface)
 
---
+++ src/morpheo/gui/MorpheoDialog.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'ui', 'morpheo_dialog.ui'))
+
+
+class MorpheoDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(MorpheoDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
